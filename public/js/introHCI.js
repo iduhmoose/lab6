8e54d565a9ed5c5e946f3cd61dc6cce299356a09
Jessica Nguyen <jdn039@ucsd.edu>
2020-02-13 11:44:05 -0800

    finished

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,5 +26,18 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	var url = "/project/" + idNumber;
+	console.log(url);
+
+	$.get(url, callBackFn);
+
 	console.log("User clicked on project " + idNumber);
+
+
+}
+
+function callBackFn(result) {
+	console.log(result);
+	var htmlstring = "<h3>"+ result.title +"</h3><p>"+ result.date +"</p><img src='" + result.image + "' class='detailsImage'> " + result.summary;
+	$("#project" + result.id + " div.details").html(htmlstring);
 }
